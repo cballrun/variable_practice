57f3f997f8671bf3077d2338c00e9e805088584f
Carter Ball <carter.k.ball@gmail.com>
2022-06-07 22:41:36 -0400

    Add strings and integer variables to variables.rb file

---
+++ variables.rb
@@ -0,0 +1,15 @@
+# Variables
+
+## String Variables
+
+p1 = "Paul Leonard"
+p2 = "Carter Ball"
+p3 = "Chase Gaddis"
+
+## Integer Variables
+
+too_old = 30
+too_young = 20
+just_right = 25
+
+## Float Variables
