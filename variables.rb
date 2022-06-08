2e6d94d1f81cb33405b1f476d48fe61fad64290c
Carter Ball <carter.k.ball@gmail.com>
2022-06-07 22:54:33 -0400

    Add reassigned variable values and put statements

--- variables.rb
+++ variables.rb
@@ -6,11 +6,15 @@ p1 = "Paul Leonard"
 p2 = "Carter Ball"
 p3 = "Chase Gaddis"
 
+puts p1
+
 ## Integer Variables
 
 too_old = 30
 too_young = 20
 just_right = 27
+
+puts just_right
 
 ## Float Variables
 
@@ -18,8 +22,31 @@ pauls_bank_account = 42.24
 carters_bank_account = 28000.37
 chases_bank_account = 10000.32
 
+puts pauls_bank_account
+
+
 ## Boolean Variables
 
 paul_too_old = true
 carter_too_young = false
 carter_just_right = true
+
+puts carter_just_right
+
+
+## Reassignments
+
+p1 = "Pail Leonard"
+just_right = 29
+pauls_bank_account = 2.33
+carter_just_right = false
+
+## Put Statements
+
+puts p1
+
+puts carter_just_right
+
+puts pauls_bank_account
+
+puts carter_just_right
